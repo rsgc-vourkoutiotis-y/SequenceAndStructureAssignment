4d5eaa4a022b3af7fea584839b3924dda32de168
Russell Gordon <rgordon@rsgc.on.ca>
2017-02-27 12:03:13 -0500

    Initial Commit

---
+++ SequenceAndStructureAssignment/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SequenceAndStructureAssignment
+//
+//  Created by Russell Gordon on 2/27/17.
+//  Copyright © 2017 Russell Gordon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
