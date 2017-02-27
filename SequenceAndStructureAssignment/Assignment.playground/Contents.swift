b62797cd1a96845314405d98b0f32c381a7e3728
Russell Gordon <rgordon@rsgc.on.ca>
2017-02-27 12:33:33 -0500

    Wrote introduction to assignment.

---
+++ SequenceAndStructureAssignment/Assignment.playground/Contents.swift
@@ -0,0 +1,36 @@
+/*:
+ # Introduction
+ 
+ ## Purpose
+ 
+ The purpose of this assignment is to create an aesthetically pleasing design using repeated visual elements overlaid on a grid.
+ 
+ ## Goal
+ 
+ Designs produced should look good on a laptop skin or t-shirt – this means avoiding very fine detail.
+ 
+ ## Deliverables
+ 
+ There are three deliverables for this assignment:
+ 
+ 1. A plan (access [template here](http://russellgordon.ca/rsgc/2016-17/ics2o/Making-a-Plan.pdf), and [exemplar here](http://russellgordon.ca/rsgc/2016-17/ics2o/Part-1-Making-a-Plan-Exemplar.pdf) )
+ 2. The product (you will use this playground to create products)
+ 3. An explanation of your code (will be created after March Break)
+ 
+ ## Reminder
+ 
+ Complete at least one plan, photograph it, and post it to Sesame.
+ 
+ Here is an [exemplar for a good plan](http://russellgordon.ca/rsgc/2016-17/ics2o/Part-1-Making-a-Plan-Exemplar.pdf). Be sure to review this before making your own plan.
+ 
+ *Your plan needs to be approved by me, Mr. Gordon, before you can begin implementing the product.*
+ 
+ The approval is solely so that I can provide you with verbal feedback to help you improve your plan before you begin implementing your ideas in code.
+ 
+ ## Evaluation
+ 
+ Here is the [rubric being used to evaluate your work](http://russellgordon.ca/rsgc/2016-17/ics2o/Sequence-and-Structure-Product-Rubric.pdf) on this assignment.
+ 
+ [Next](@next)
+ 
+ */
