37fa913bf4e3a8d0effc1ab2b55ab41b36d854e3
Student <student@Yanni-Vourkoutiotis.local>
2017-03-03 09:48:09 -0500

    Starting to work on my agent based project

--- SequenceAndStructureAssignment/Assignment.playground/Pages/Design One.xcplaygroundpage/Contents.swift
+++ SequenceAndStructureAssignment/Assignment.playground/Pages/Design One.xcplaygroundpage/Contents.swift
@@ -21,26 +21,115 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 500, height: 500)
 
-// Generate a grid
-canvas.drawShapesWithFill = false
-canvas.defaultBorderWidth = 1
+// No borders
+canvas.drawShapesWithBorders = false
 
+// Declare constants
+let north = 0
+let northEast = 1
+let east = 2
+let southEast = 3
+let south = 4
+let southWest = 5
+let west = 6
+let northWest = 7
 
-// This loop makes a 10 rows of columns
-for x in stride(from: 25, through: 475, by: 50){
+// Set the step size (travel distance per iteration)
+let stepSize = 1
+
+// Set the pixel size
+let diameter = 10
+
+// Generate a starting position
+var x = random(from: 50, toButNotIncluding: canvas.width - 50)
+var y = random(from: 50, toButNotIncluding: canvas.height - 50)
+
+// Travel 500 steps
+for _ in 1...500 {
     
-    // This loop makes a single column, bottom to top
-    for y in stride(from: 25, through: 475, by: 50) {
-        
-        // Draw the shapes
-        canvas.drawEllipse(centreX: x, centreY: y, width: 2, height: 2)
-        canvas.drawRectangle(centreX: x, centreY: y, width: 50, height: 50)
+    // Generate a random direction
+    var direction = random(from: 0, toButNotIncluding: 8)
+    
+    // Reverse direction if edge reached
+    if x + stepSize > canvas.width {
+        direction = west
     }
-}
+    if x - stepSize < 0 {
+        direction = east
+    }
+    if y + stepSize > canvas.height {
+        direction = south
+    }
+    if y - stepSize < 0 {
+        direction = north
+    }
+    
+    // Change position based on direction and step size
+    if direction == north {
+        y = y + stepSize
+    } else if direction == northEast {
+        x = x + stepSize
+        y = y + stepSize
+    } else if direction == east {
+        x = x + stepSize
+    } else if direction == southEast {
+        x = x + stepSize
+        y = y - stepSize
+    } else if direction == south {
+        y = y - stepSize
+    } else if direction == southWest {
+        x = x - stepSize
+        y = y - stepSize
+    } else if direction == west {
+        x = x - stepSize
+    } else if direction == northWest {
+        x = x - stepSize
+        y = y + stepSize
+    }
+    
+    // Draw the agent in it's new position
+    canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 0, alpha: 25)
+    canvas.drawEllipse(centreX: x, centreY: y, width: diameter, height: diameter)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
  */
-PlaygroundPage.current.liveView = canvas.imageView
+    PlaygroundPage.current.liveView = canvas.imageView}
